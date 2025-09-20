6dfe0586eecd27953ac58eae827146e0305861f7
Emmanuel-Antibiotic <projectpalng@gmail.com>
2025-09-20 18:29:39 +0100

    Changing some defaults

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <p>Hello 👋 I’m learning React from Figma and today's date is what exactly!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeroSection />
     </div>
   );
 }
