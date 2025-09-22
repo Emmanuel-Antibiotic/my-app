b5784785bd31855bfdaeec699d731a8117cbc847
Emmanuel-Antibiotic <projectpalng@gmail.com>
2025-09-22 19:45:12 +0100

    nn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import { HeroSection } from "./Components/HeroSection";
+import HeroSection from "./Components/HeroSection";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HeroSection />
+      <HeroSection/>
     </div>
     
   );
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Menu, X, Stethoscope, Calendar, Video, Users, Leaf, BookOpen, ShoppingBag } from "lucide-react";
-
-export default function Home() {
+export default function HeroSection() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
